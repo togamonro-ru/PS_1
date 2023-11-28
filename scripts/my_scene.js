274681c8aaab88e8ed1a4333d55db5d0bb9862c9
togamonro-ru <a.togasawa.sys22@morijyobi.ac.jp>
2023-11-28 15:01:28 +0900

    45度移動

--- scripts/my_scene.js
+++ scripts/my_scene.js
@@ -11,20 +11,31 @@ class MyScene extends Phaser.Scene {
     // シーンの事前読み込み処理
     preload() {
          // 画像の読み込み(使用する時の名前, パス)
-        this.load.image('sky', 'assets/sky.png');
-        this.load.image('alien1', 'assets/alien1.png');
+        this.load.image('background', 'assets/background.png');
+        this.load.image('taro', 'assets/taro.png');
     }
 
     // シーン初期化処理
     create() {
          // 単体画像をシーンに追加(X座標,Y座標,画像名)
-        this.add.image(D_WIDTH/2, D_HEIGHT/2, 'sky');
-        this.add.image(D_WIDTH/2, D_HEIGHT/2, 'alien1');
+        this.add.image(D_WIDTH/2, D_HEIGHT/2, 'background');
+        this.player1 = this.add.image(500, 350, 'taro');
+        this.player1_direction = 1;
         this.text = this.add.text(10, 10, 'Scene 1').setFontSize(32).setColor('#ff0');
     }
     
   // 毎フレーム実行される繰り返し処理
     update() {
+        if (this.player1.x >= D_WIDTH - 100) this.player1_direction = -1;
+        if (this.player1.x <= 100) this.player1_direction = 1;
+
+        if (this.player1_direction == 1) {
+            this.player1.x += 5;// 横方向へ移動を設定
+            this.player1.y += 5;
+        } else {
+            this.player1.x -= 5;// 横方向へ移動を設定
+            this.player1.y -= 5;
+        }
     }
 
 }
