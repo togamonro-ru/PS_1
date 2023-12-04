c8f6bbb2d89db406a1070b31eb3a1313aa65fe86
togamonro-ru <a.togasawa.sys22@morijyobi.ac.jp>
2023-12-04 14:02:31 +0900

    1-5

--- scripts/my_scene.js
+++ scripts/my_scene.js
@@ -13,6 +13,7 @@ class MyScene extends Phaser.Scene {
          // 画像の読み込み(使用する時の名前, パス)
         this.load.image('background', 'assets/background.png');
         this.load.image('taro', 'assets/taro.png');
+        this.load.image('jori', 'assets/jiro.png');
     }
 
     // シーン初期化処理
@@ -21,22 +22,40 @@ class MyScene extends Phaser.Scene {
         this.add.image(D_WIDTH/2, D_HEIGHT/2, 'background');
         this.player1 = this.add.image(500, 350, 'taro');
         this.player1_direction = 1;
+        this.player2 = this.add.image(600, 350, 'jori');
+        this.player2_direction = 1;
         this.text = this.add.text(10, 10, 'Scene 1').setFontSize(32).setColor('#ff0');
     }
     
   // 毎フレーム実行される繰り返し処理
     update() {
-        if (this.player1.x >= D_WIDTH - 50) this.player1_direction = -1;
-        if (this.player1.x <= 50) this.player1_direction = 1;
+        // if (this.player1.x >= D_WIDTH - 50) this.player1_direction = -1;
+        // if (this.player1.x <= 50) this.player1_direction = 1;
 
-        if (this.player1_direction == 1) {
-            this.player1.x += 5;// 横方向へ移動を設定
-            this.player1.y += 5;
-            this.player1.angle += 5;
-        } else {
-            this.player1.x -= 5;// 横方向へ移動を設定
-            this.player1.y -= 5;
-            this.player1.angle += 5;
+        // if (this.player1_direction == 1) {
+        //     this.player1.x += 5;// 横方向へ移動を設定
+        //     this.player1.y += 5;
+        //     this.player1.angle += 5;
+        // } else {
+        //     this.player1.x -= 5;// 横方向へ移動を設定
+        //     this.player1.y -= 5;
+        //     this.player1.angle += 5;
+        // }
+        // キーボードの情報を取得
+
+        let cursors = this.input.keyboard.createCursorKeys();
+        if (cursors.up.isDown) {
+        this.player1.y -= 3;// 上方向に移動
+        } else if (cursors.down.isDown) {
+        this.player1.y += 3;// 下方向に移動
+        } else if (cursors.left.isDown) {
+        //console.log("Left!");
+        this.player1.x -= 50;// 右方向に移動
+        this.player2.x -= 50;// 左方向に移動
+        } else if (cursors.right.isDown) {
+        //console.log("Right!");
+        this.player1.x += 50;// 右方向に移動
+        this.player2.x += 50;// 右方向に移動
         }
     }
 
