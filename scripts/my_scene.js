9e9c8db02b7b5afd0fa4f876376686aaba875659
togamonro-ru <a.togasawa.sys22@morijyobi.ac.jp>
2023-12-04 15:00:22 +0900

    1-7

--- scripts/my_scene.js
+++ scripts/my_scene.js
@@ -10,26 +10,26 @@ class MyScene extends Phaser.Scene {
 
     // シーンの事前読み込み処理
     preload() {
-         // 画像の読み込み(使用する時の名前, パス)
+        // 画像の読み込み(使用する時の名前, パス)
         this.load.image('background', 'assets/background.png');
         this.load.image('taro', 'assets/taro.png');
-        this.load.image('jori', 'assets/jiro.png');
+        this.load.image('jori', 'assets/taro.png');
+        this.load.image('hanako', 'assets/hanako.png');
     }
 
     // シーン初期化処理
     create() {
-         // 単体画像をシーンに追加(X座標,Y座標,画像名)
-         this.add.image(D_WIDTH/2, D_HEIGHT/2, 'background');
-         this.player1 = this.add.image(500, 350, 'taro');
-         this.player1_direction = 1;
-         this.player2 = this.add.image(600, 350, 'jori');
-         this.player2_direction = 1;
+        // 単体画像をシーンに追加(X座標,Y座標,画像名)
+        this.add.image(D_WIDTH / 2, D_HEIGHT / 2, 'background');
+        this.text = this.add.text(600, 400, 'MyWorld').setFontSize(32).setColor('#ff0');
+        this.player = this.add.image(D_WIDTH / 2, D_HEIGHT / 2, 'taro');
+        this.player1 = this.add.image(D_WIDTH / 2, D_HEIGHT / 2, 'jori');
 
+        // プレイヤーの移動方向フラグを設定　１：右向き　−１：左向き
+        // this.player_direction = 1;
+        // this.player.angle = 0
 
-         this.text = this.add.text(10, 10, 'Scene 1').setFontSize(32).setColor('#ff0');
-        this.Text = this.add.text(600, 400, 'MyWorld', { fontSize: '28px', fill: '#FFF' ,fontFamily: "Arial"});
-        this.a_Text = this.add.text(100, 50, '', { fontSize: '28px', fill: '#FFF' ,fontFamily: "Arial"});
-        this.s_Text = this.add.text(100, 50, '', { fontSize: '28px', fill: '#FFF' ,fontFamily: "Arial"});
+        this.text = this.add.text(100, 50, '').setFontSize(32).setColor('#ff0');
 
         ///WASDキーを検知できるようにする
         this.keys = {};
@@ -38,49 +38,45 @@ class MyScene extends Phaser.Scene {
         this.keys.keyS = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S);
         this.keys.keyD = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
     }
-    
-  // 毎フレーム実行される繰り返し処理
-    update() {
-        // if (this.player1.x >= D_WIDTH - 50) this.player1_direction = -1;
-        // if (this.player1.x <= 50) this.player1_direction = 1;
 
-        // if (this.player1_direction == 1) {
-        //     this.player1.x += 5;// 横方向へ移動を設定
-        //     this.player1.y += 5;
-        //     this.player1.angle += 5;
+    // 毎フレーム実行される繰り返し処理
+    update() {
+        // // プレイヤーの向きフラグを変更
+        // if (this.player.y >= D_HEIGHT - 100) this.player_direction = -1;
+        // if (this.player.y <= 100) this.player_direction = 1;
+        // // プレイヤーの移動
+        // if (this.player_direction == 1) {
+        //     this.player.y -= 5;// 横方向へ移動を設定
+        //     this.player.x += 5;// 横方向へ移動を設定
+        //     // 回転角度を更新
+        //     this.player.angle += 5;
+        //     // 回転角度を設定
+        //     this.player.setAngle(this.player.angle);
         // } else {
-        //     this.player1.x -= 5;// 横方向へ移動を設定
-        //     this.player1.y -= 5;
-        //     this.player1.angle += 5;
+        //     this.player.setVelocityX(0);// 横方向の速度を0
+        //     this.player.setVelocityY(0);// 縦方向の速度を0
         // }
-        // キーボードの情報を取得
 
         let cursors = this.input.keyboard.createCursorKeys();
-        if (cursors.up.isDown) {
-        this.player1.y -= 3;// 上方向に移動
-        } else if (cursors.down.isDown) {
-        this.player1.y += 3;// 下方向に移動
-        } else if (cursors.left.isDown) {
-        //console.log("Left!");
-        this.player1.x -= 50;// 右方向に移動
-        this.player2.x += 50;// 左方向に移動
+        if (cursors.left.isDown) {
+            this.player.x -= 5;
+            this.player1.x += 5;
         } else if (cursors.right.isDown) {
-        //console.log("Right!");
-        this.player1.x += 50;// 右方向に移動
-        this.player2.x -= 50;// 右方向に移動
+            this.player.x += 5;
+            this.player1.x -= 5;
         }
-        this.wasd_move(this.keys, this.a_Text);
-        this.wasd_move(this.keys, this.s_Text);
-    }
-    
-    wasd_move(keys, object){
-        if(keys.keyS.isDown){
-            this.s_Text.setText('Hey!');
-        }else if(keys.keyA.isDown){
-            this.a_Text.setText('Hello!');
-        }else if(keys.keyD.isDown){
-            this.a_Text.setText('');
-            this.s_Text.setText('');
+
+        if (this.keys.keyA.isDown) {
+            this.text.setText('Hello!'); // テキストを設定
+        } else if (this.keys.keyS.isDown) {
+            this.text.setText('Hey!'); // テキストを設定
+        } else if (this.keys.keyD.isDown) {
+            this.text.setText(''); // テキストを空にする
+        }
+
+         if (this.keys.keyW.isDown) {
+             this.player2 = this.add.image(100, 100, 'hanako');
+             this.player2.x = Phaser.Math.Between(100, 400);
         }
     }
 }
